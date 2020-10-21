fad280814309b2d39a49c73eeed0e024b7a553a2
recall-lidemin <1425889927@qq.com>
2020-10-21 22:14:29 +0800

    观察者模式

--- src/proxy/index.js
+++ src/proxy/index.js
@@ -27,3 +27,37 @@ proxy.display()
 // 1.网页事件代理(事件委托)
 // 2.jQuery $.proxy
 // 3.ES6 Proxy
+let star = {
+  name: '张XX',
+  age: 25,
+  phone: '1390000011',
+}
+
+let agent = new Proxy(star, {
+  get: (target, key) => {
+    if (key === 'phone') {
+      // 返回经纪人电话
+      return '1866666666'
+    }
+    if (key === 'price') {
+      return 120000
+    }
+    return target[key]
+  },
+  set: (target, key, val) => {
+    if (key === 'customPrice') {
+      if (val < 100000) {
+        throw Error('价格太低')
+      } else {
+        target[key] = val
+        return true
+      }
+    }
+  },
+})
+
+console.log(agent.name)
+console.log(agent.phone)
+console.log(agent.price)
+console.log((agent.customPrice = 900000))
+console.log((agent.customPrice = 90000))
